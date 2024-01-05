4376f12b69a9d2040512eea392d232d51270d7f4
2213saabji <2213saabji@gmail.com>
2024-01-06 04:02:22 +0530

    first

--- app/[coinId]/page.js
+++ app/[coinId]/page.js
@@ -1,22 +1,31 @@
 'use client'
 import React, { useState, useEffect } from 'react';
 
-function CoinDetail() {
+function CoinDetail({params}) {
     const [coin, setCoin] = useState(null);
-
+    useEffect(()=>{
+        async function fetchdata(){
+            const response=await( await fetch("https://api.coinlore.net/api/tickers/")).json();
+            setCoin(response.data[params.coinId]);
+        }
+        fetchdata();
+    },[params])
     return (
         <div className='coin-detail'>
+            {coin &&<>
             <div className='name-symbol'>
-                <h1 className='name'>coin-name</h1>
-                <h2 className='symbol'>(coin-symbol)</h2>
+                <h1 className='name'>{coin.name}</h1>
+                <h2 className='symbol'>({coin.symbol})</h2>
             </div>
             <div className='market-description'>
-                <p className='coin-rank'>Rank: coin-symbol</p>
-                <p className='coin-price'>Price: $coin-price</p>
-                <p className='coin-market-cap'>Market Cap: $coin-market_cap_usd</p>
-                <p className='coin-total-supply'>Total Supply: coin-tsupply</p>
-                <p className='coin-max-supply'>Max Supply: coin-msupply</p>
+                <p className='coin-rank'>Rank: {coin.rank}</p>
+                <p className='coin-price'>Price: ${coin.price_usd}</p>
+                <p className='coin-market-cap'>Market Cap: ${coin.csupply}</p>
+                <p className='coin-total-supply'>Total Supply: {coin.tsupply}</p>
+                <p className='coin-max-supply'>Max Supply: {coin.msupply}</p>
             </div>
+            </>
+}
         </div>
     )
 }
--- app/page.js
+++ app/page.js
@@ -4,13 +4,19 @@ import CoinCard from './components/CoinCard';
 
 function Home() {
     const [coins, setCoins] = useState([]);
-
+    useEffect(()=>{
+        async function fetchdata(){
+            const response=await( await fetch("https://api.coinlore.net/api/tickers/")).json();
+            setCoins(response.data);
+        }
+        fetchdata();
+    },[])
     return (
         <div className='home'>
             <h1>Top 20 Cryptos</h1>
             <div className='coins-container'>
                 {coins.map((coin) => (
-                    <CoinCard />
+                    <CoinCard coin={coin} />
                 ))}
             </div>
         </div>
